395243249d51dc0385abb4f4f0832cdf6ef48c0c
Suryoday <dwivedisuryodaydwivedi@gmail.com>
2026-05-23 01:05:30 +0530

    First commit

---
+++ python.py/python4.py
@@ -0,0 +1,14 @@
+# if-else conditional statements
+a = 45
+b =54
+if a > b:
+    print("a is greater than b")
+else:
+    print("b is greater than a")
+
+# check given number is above 20 or not
+num = int(input("enter your number is : "))
+if num > 20:
+    print("number is above 20") 
+else:
+    print("number is below 20")
